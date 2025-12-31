9df11564eefddcffb0bfe0868f77f7464cb38bf8
Srijani Dey <151017602+ssreyz@users.noreply.github.com>
2025-12-31 23:10:54 +0530

    Update Wi-Fi credentials and AWS IoT details

--- esp32_code/certs.h
+++ esp32_code/certs.h
@@ -6,22 +6,21 @@
 // ------------------ Thing & Wi-Fi ------------------
 
 // AWS IoT Thing Name
-#define THING_NAME "esp32_1"
+#define THING_NAME "YOUR THINGS NAME"
 
 // Wi-Fi Credentials
-const char WIFI_SSID[] = "OPPO";
-const char WIFI_PASSWORD[] = "panda123";
+const char WIFI_SSID[] = "ur wifi name";
+const char WIFI_PASSWORD[] = "password";
 
 // AWS IoT Endpoint
 const char AWS_IOT_ENDPOINT[] =
-  "a3ef8g4t9p41mb-ats.iot.us-east-1.amazonaws.com";
+  "xxxats.iot.us-east-1.amazonaws.com";
 
 // ------------------ Certificates ------------------
 
 // Amazon Root CA 1
 static const char AWS_ROOT_CA[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
-MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
 ...
 -----END CERTIFICATE-----
 )KEY";
@@ -29,7 +28,6 @@ MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
 // Device Certificate
 static const char DEVICE_CERT[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
-MIIDWTCCAkGgAwIBAgIUe5deddEKMPwuV4yJWAudIeZNOVYwDQYJKoZIhvcNAQEL
 ...
 -----END CERTIFICATE-----
 )KEY";
@@ -37,7 +35,6 @@ MIIDWTCCAkGgAwIBAgIUe5deddEKMPwuV4yJWAudIeZNOVYwDQYJKoZIhvcNAQEL
 // Device Private Key
 static const char DEVICE_PRIVATE_KEY[] PROGMEM = R"KEY(
 -----BEGIN RSA PRIVATE KEY-----
-MIIEogIBAAKCAQEAnMGKRDgV1TeqkSDgTaVjop179sfR2eMZ18R/fnINYnd8rR8n
 ...
 -----END RSA PRIVATE KEY-----
 )KEY";
